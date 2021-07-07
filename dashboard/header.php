69f81261254f4472624ddc028607096cae83a092
Rahul Kurmi <60813071+rahul01-12@users.noreply.github.com>
2021-07-07 19:35:01 +0530

    Update header.php

--- dashboard/header.php
+++ dashboard/header.php
@@ -9,7 +9,7 @@
     
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
     
-    <link href="http://code.ionicframework.com/ionicons/2.0.0/css/ionicons.min.css" rel="stylesheet" type="text/css" />    
+    <link href="https://code.ionicframework.com/ionicons/2.0.0/css/ionicons.min.css" rel="stylesheet" type="text/css" />    
     
     <link href="../dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css" />
     
