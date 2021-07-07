1f5e14da9c8843ce4bdd5eba5dea092f899e7dbb
Rahul Kurmi <60813071+rahul01-12@users.noreply.github.com>
2021-07-07 19:35:46 +0530

    Update footer.php

--- dashboard/footer.php
+++ dashboard/footer.php
@@ -9,7 +9,7 @@
     
     <script src="../plugins/jQuery/jQuery-2.1.3.min.js"></script>
     
-    <script src="http://code.jquery.com/ui/1.11.2/jquery-ui.min.js" type="text/javascript"></script>
+    <script src="https://code.jquery.com/ui/1.11.2/jquery-ui.min.js" type="text/javascript"></script>
     
     <script>
       $.widget.bridge('uibutton', $.ui.button);
@@ -17,7 +17,7 @@
    
     <script src="../bootstrap/js/bootstrap.min.js" type="text/javascript"></script>    
     
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
     <script src="plugins/morris/morris.min.js" type="text/javascript"></script>
    
     <script src="../plugins/sparkline/jquery.sparkline.min.js" type="text/javascript"></script>
