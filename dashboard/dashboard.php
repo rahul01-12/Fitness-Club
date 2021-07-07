4c6a7f68c4aaa81d342daf069e168c63c78c7098
Rahul Kurmi <60813071+rahul01-12@users.noreply.github.com>
2021-07-07 19:30:06 +0530

    Update dashboard.php

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -13,7 +13,7 @@
             <small>Control panel</small>
           </h1>
           <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Home</a></li>
             <li class="active">Dashboard</li>
           </ol>
         </section>
